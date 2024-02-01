e2556c25559d1db523a4c5a7d055477c3a8eed3b
baconbc <baconbcr@yahoo.com>
2024-02-01 00:33:25 -0800

    Created base game
    - Player that can move with animations
    - Basic tilemap

---
+++ GameJam2024/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private int speed = 5;
+
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnMovement(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsMoving", true);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+    }
+
+    private void OnToggleInventory(InputValue value)
+    {
+        Debug.Log("toggle inv");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime); // use fixedDeltaTime instead of deltaTime cuz in FixedUpdate() not Update()
+    }
+}
